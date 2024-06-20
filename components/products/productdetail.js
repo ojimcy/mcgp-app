b58e343d9ba324fd367e7bd127932cf2d655c624
vs45 <bringfirehjake88@gmail.com>
2024-06-20 12:14:38 +0100

    clean up on product detail

--- components/products/productdetail.js
+++ components/products/productdetail.js
@@ -1,19 +1,33 @@
-import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
-import { COLORS} from '../../constants';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { COLORS } from "../../constants";
 
-const ProductDetail = ({ image,companyName,title,description,location,price,phone }) => {
-  const [isMore,setIsMore]=useState(false);
+const ProductDetail = ({
+  image,
+  companyName,
+  title,
+  description,
+  location,
+  price,
+  phone,
+}) => {
+  const [isMore, setIsMore] = useState(false);
   return (
     <ScrollView style={styles.container}>
-      <Image
-        source={{ uri: image }} 
-        style={styles.image}
-      />
+      <Image source={{ uri: image }} style={styles.image} />
       <View style={styles.contentContainer}>
         <Text style={styles.title}>
-          {companyName}{"'s "}{title}
-          </Text>
+          {companyName}
+          {"'s "}
+          {title}
+        </Text>
         <View style={styles.locationContainer}>
           <Text style={styles.location}>📍 {location}</Text>
           <Text style={styles.rating}>4.7</Text>
@@ -29,38 +43,39 @@ const ProductDetail = ({ image,companyName,title,description,location,price,phon
           <Text style={styles.productInfoTitle}>Call to place order</Text>
           <Text style={styles.productInfoValue}>{phone}</Text>
         </View>
-        {!isMore&& <TouchableOpacity
-        onPress={()=>setIsMore(true)}
-        style={styles.viewMoreButton}>
-          <Text style={styles.viewMoreText}>View more</Text>
-        </TouchableOpacity>}
-        {isMore&&
+        {!isMore && (
+          <TouchableOpacity
+            onPress={() => setIsMore(true)}
+            style={styles.viewMoreButton}
+          >
+            <Text style={styles.viewMoreText}>View more</Text>
+          </TouchableOpacity>
+        )}
+        {isMore && (
           <>
-          <Text style={styles.descriptionTitle}>Description</Text>
-        <Text style={styles.descriptionText}>
-          {description}
-           </Text>
+            <Text style={styles.descriptionTitle}>Description</Text>
+            <Text style={styles.descriptionText}>{description}</Text>
           </>
-        }
-        
+        )}
       </View>
-      {isMore&& 
-      <>
-      <View style={styles.footer}>
-
-        <Text style={styles.reviewTitle}>Verified customer Feedback</Text>
-        <Text style={styles.reviewRating}>Product rating and review</Text>
-        <Text style={styles.reviewScore}>4.7/5</Text>
-        <Text style={styles.reviewCount}>500 rating so far</Text>
-        <Text style={styles.reviewDate}>18/6/2024</Text>
-        <Text style={styles.reviewAuthor}>Cynthia</Text>
-      </View>
-      <TouchableOpacity onPress={()=>setIsMore(false)} style={styles.viewLessButton}>
-      <Text style={styles.viewMoreText}>View less</Text>
-    </TouchableOpacity>
-    </>
-      }
-     
+      {isMore && (
+        <>
+          <View style={styles.footer}>
+            <Text style={styles.reviewTitle}>Verified customer Feedback</Text>
+            <Text style={styles.reviewRating}>Product rating and review</Text>
+            <Text style={styles.reviewScore}>4.7/5</Text>
+            <Text style={styles.reviewCount}>500 rating so far</Text>
+            <Text style={styles.reviewDate}>18/6/2024</Text>
+            <Text style={styles.reviewAuthor}>Cynthia</Text>
+          </View>
+          <TouchableOpacity
+            onPress={() => setIsMore(false)}
+            style={styles.viewLessButton}
+          >
+            <Text style={styles.viewMoreText}>View less</Text>
+          </TouchableOpacity>
+        </>
+      )}
     </ScrollView>
   );
 };
@@ -69,25 +84,25 @@ export default ProductDetail;
 
 const styles = StyleSheet.create({
   container: {
-   
     backgroundColor: COLORS.white,
   },
   image: {
-    width: '100%',
+    width: "100%",
     height: 300,
-    resizeMode:'cover'
+    resizeMode: "cover",
   },
   contentContainer: {
     padding: 20,
   },
   title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
+    fontFamily: "Inter",
+    fontSize: 14,
+    fontWeight: 500,
+    lineHeight: 22,
   },
   locationContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 10,
   },
   location: {
@@ -97,36 +112,36 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   ratingCount: {
-    color: 'gray',
+    color: "gray",
   },
   priceContainer: {
     marginBottom: 10,
   },
   price: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 5,
   },
   shipping: {
-    color: 'gray',
+    color: "gray",
   },
   productInfoContainer: {
     marginBottom: 20,
   },
   productInfoTitle: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   productInfoValue: {
     fontSize: 16,
     marginBottom: 10,
   },
   viewMoreButton: {
-    alignItems: 'center',
+    alignItems: "center",
     marginVertical: 10,
   },
   viewLessButton: {
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 50,
   },
   viewMoreText: {
@@ -134,7 +149,7 @@ const styles = StyleSheet.create({
   },
   descriptionTitle: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
   descriptionText: {
@@ -147,7 +162,7 @@ const styles = StyleSheet.create({
   },
   reviewTitle: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 5,
   },
   reviewRating: {
@@ -156,7 +171,7 @@ const styles = StyleSheet.create({
   },
   reviewScore: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 5,
   },
   reviewCount: {
@@ -171,4 +186,3 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
 });
-
