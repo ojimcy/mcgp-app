9bcaaf528dc4e811dea89500f67544a6b704587e
vs45 <bringfirehjake88@gmail.com>
2024-06-21 23:38:46 +0100

    order card design

--- components/orders/ordercard.js
+++ components/orders/ordercard.js
@@ -10,9 +10,26 @@ const OrderCard = ({ order }) => {
   return (
     <View style={styles.card}>
       <Text style={styles.title}>Order ID: {order.id}</Text>
-      <Text style={styles.text}>Payment Status: {order.paymentStatus}</Text>
-      <Text style={styles.text}>Delivery Address: {order.deliveryAddress.fullName}, {order.deliveryAddress.address}, {order.deliveryAddress.city}, {order.deliveryAddress.state}, {order.deliveryAddress.country}</Text>
-      <Text style={styles.text}>Phone: {order.deliveryAddress.phoneNumber}</Text>
+      <View style={{flexDirection:'row'}}>
+      <Text style={styles.label}>Amount: </Text>
+      <Text style={styles.text}>{order.amount}</Text>
+      </View>
+      <View style={{flexDirection:'row'}}>
+      <Text style={styles.label}>Payment Status:</Text>
+      <Text  style={[
+            styles.text, 
+            order.paymentStatus === 'Pending' ? styles.pending : styles.completed
+          ]}> {order.paymentStatus}</Text>
+      </View>
+      <View style={{flexDirection:'col'}}>
+      <Text style={styles.label}>Delivery Address: </Text>
+      <Text style={styles.text}>{order.deliveryAddress.fullName}, {order.deliveryAddress.address}, {order.deliveryAddress.city}, {order.deliveryAddress.state}, {order.deliveryAddress.country}</Text>
+      </View>
+      <View style={{flexDirection:'row'}}>
+      <Text style={styles.label}>Phone:</Text>
+      <Text style={styles.text}> {order.deliveryAddress.phoneNumber}</Text>
+      </View>
+      
       <FlatList
         data={order.product}
         keyExtractor={(item, index) => index.toString()}
@@ -20,7 +37,7 @@ const OrderCard = ({ order }) => {
           <View style={styles.product}>
             <Image source={{ uri: item.image }} style={styles.productImage} />
             <Text style={styles.productText}>Price: {item.price}</Text>
-            <Text style={styles.productText}>Quantity: {item.quantity}</Text>
+            <Text style={styles.productText}>Qty: {item.quantity}</Text>
           </View>
         )}
       />
@@ -59,6 +76,11 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 10,
   },
+  label: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginRight: 5,
+  },
   text: {
     fontSize: 14,
     marginBottom: 5,
@@ -67,6 +89,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: 10,
+  
   },
   productImage: {
     width: 50,
@@ -75,6 +98,13 @@ const styles = StyleSheet.create({
   },
   productText: {
     fontSize: 14,
+    padding:5
+  },
+  pending: {
+    color: 'red',
+  },
+  completed: {
+    color: 'green',
   },
 });
 
