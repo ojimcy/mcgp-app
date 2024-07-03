d739bf0cf076b6b728ad2f9b563a4464b114eef2
vs45 <bringfirehjake88@gmail.com>
2024-07-03 10:42:51 +0100

    latest update

--- components/orders/delivery.js
+++ components/orders/delivery.js
@@ -61,7 +61,7 @@ const Delivery = ({ data }) => {
     try {
       const response = await fetch(
         "https://countriesnow.space/api/v0.1/countries/state/cities",
-        {
+        {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
           method: "POST",
           headers: {
             "Content-Type": "application/json",
