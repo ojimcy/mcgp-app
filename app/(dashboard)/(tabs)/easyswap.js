72dcdc48660e7b47cabc228484c099cf061f464f
vs45 <bringfirehjake88@gmail.com>
2024-07-04 13:01:41 +0100

    coming soon notification

--- app/(dashboard)/(tabs)/easyswap.js
+++ app/(dashboard)/(tabs)/easyswap.js
@@ -1,17 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React,{useEffect} from 'react'
-import * as NavigationBar from 'expo-navigation-bar';
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import * as NavigationBar from "expo-navigation-bar";
 const easyswap = () => {
-    useEffect(()=>{
-        
-    })
+  useEffect(() => {});
   return (
-    <View>
-      <Text>easyswap</Text>
+    <View
+      style={{
+        alignContent: "center",
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1,
+      }}
+    >
+      <Text style={{ padding: 10, fontSize: 16, fontWeight: "600" }}>
+        Easy Swap Coming soon
+      </Text>
     </View>
-  )
-}
+  );
+};
 
-export default easyswap
+export default easyswap;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- app/(dashboard)/(tabs)/tokenization.js
+++ app/(dashboard)/(tabs)/tokenization.js
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 const Tokenization = () => {
   return (
-    <View>
-      <Text>Tokenization</Text>
+    <View
+      style={{
+        alignContent: "center",
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1,
+      }}
+    >
+      <Text style={{ padding: 10, fontSize: 16, fontWeight: "600" }}>
+        Tokenization coming soon
+      </Text>
     </View>
-  )
-}
+  );
+};
 
-export default Tokenization
+export default Tokenization;
